f8859caf37d560bd40b7cf133a414bd8a5793f95
EstebanGC <johan.gal.car@gmail.com>
2024-02-29 21:32:44 -0500

    Building threads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,32 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.activeCount());
-//        Thread.currentThread().setName("Practice0");
-//        System.out.println(Thread.currentThread().getName());
-//        Thread.currentThread().setPriority(1);
-//        System.out.println(Thread.currentThread().getPriority());
+        MyThread thread1 = new MyThread();
 
-//        System.out.println(Thread.currentThread().isAlive());
 
-//        for (int i = 3; i > 0; i--) {
-//            System.out.println(i);
-//            Thread.sleep(1000);
-//        }
-//        System.out.println("You are done");
-
-        MyThread thread2 = new MyThread();
-
-        thread2.setDaemon(false);
-        System.out.println(thread2.isDaemon());
-        thread2.start();
-//        System.out.println(thread2.isAlive());
-//        thread2.setName("2nd thread");
-//        System.out.println(thread2.getName());
-//        thread2.setPriority(1);
-//        System.out.println(thread2.getPriority());
-
-        System.out.println(Thread.activeCount());
     }
 }
 
--- src/main/java/org/example/MyThread.java
+++ src/main/java/org/example/MyThread.java
@@ -4,10 +4,19 @@ public class MyThread extends Thread {
     @Override
     public void run(){
 
-        if(this.isDaemon()) {
-            System.out.println("This thread is running!");
-        } else {
-            System.out.println("This is a user thread that is running!");
+        for(int i = 10; i > 0; i--) {
+            System.out.println("Thread #1: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println("Thread #1 is finished: ");
+//        if(this.isDaemon()) {
+//            System.out.println("This thread is running!");
+//        } else {
+//            System.out.println("This is a user thread that is running!");
+//        }
     }
 }
